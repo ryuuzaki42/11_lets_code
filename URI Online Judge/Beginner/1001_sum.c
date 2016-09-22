cc44f9ac52efcf00e9f5b70abcd4032181f3f9a1
João Batista <joao42lbatista@gmail.com>
2016-09-22 14:10:27 -0300

    problem Beginner 1001, sum to number and output X= 'sum'

---
+++ URI Online Judge/Beginner/1001_sum.c
@@ -0,0 +1,18 @@
+// Read 2 integer values (a, b), make the sum, and its result to X
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B, X;
+
+    cin >> A >> B;
+
+    X = A + B;
+
+    cout << "X = " << X << endl;
+
+    return 0;
+}
+
