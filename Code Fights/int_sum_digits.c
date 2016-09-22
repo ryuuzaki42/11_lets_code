7b744908a4d23a22c6c2f158670b577c3fc958d8
João Batista <joao42lbatista@gmail.com>
2016-09-22 14:42:30 -0300

    Add int_sum_digits

---
+++ Code Fights/int_sum_digits.c
@@ -0,0 +1,28 @@
+// Given an integer, find the sum of all its digits
+// n = 111 =>  3 : 1 + 1 + 1
+// n = 172 => 10 : 1 + 7 + 2
+
+// # Code credits: Clebson Cardoso [my friend :)] => https://github.com/clebsonc #
+
+#include <iostream>
+
+using namespace std;
+
+int sum_digits(int n) {
+    int sum = 0;
+    while (n != 0){
+        sum += n % 10;
+        n = n/10;
+   }
+   return sum;
+}
+
+int main(){
+    int n =0;
+
+    cin >> n;
+
+    cout << sum_digits(n) << endl;
+
+    return 0;
+}
