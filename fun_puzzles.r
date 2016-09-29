a5a28c8f00d1af340943e2e144534a080fdef96c
João Batista <joao42lbatista@gmail.com>
2016-09-29 10:38:59 -0300

    Added fun_puzzles.r

---
+++ fun_puzzles.r
@@ -0,0 +1,3 @@
+1) A spider is trying to build a web for itself. The web built by it doubles everyday.
+   If the spider completely built the web in 15 days, how many days did it take for the spider to build 25% of the web?
+    R: 13
